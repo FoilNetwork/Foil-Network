c4df92caab9180d04b4831014fad782e97cbf698
icreator <icreator@mail.ru>
2022-12-11 09:11:45 +0300

    Voola

--- src/main/java/org/erachain/dapp/epoch/Voucher.java
+++ src/main/java/org/erachain/dapp/epoch/Voucher.java
@@ -104,8 +104,8 @@ public class Voucher extends EpochDAPPjson {
                 boolean isUnTransferable = false;
 
                 boolean isAnonimDenied = false;
-                if (coins.equals("DOGE") && amount.compareTo(new BigDecimal("500")) > 0
-                        || coins.equals("USD") && amount.compareTo(new BigDecimal("1000")) > 0) {
+                if (false && (coins.equals("DOGE") && amount.compareTo(new BigDecimal("500")) > 0
+                        || coins.equals("USD") && amount.compareTo(new BigDecimal("1000")) > 0)) {
                     isAnonimDenied = true;
                 }
 
@@ -121,7 +121,11 @@ public class Voucher extends EpochDAPPjson {
 
                 //JSONArray array = new JSONArray();
                 //array.add(coins); array.add(amount);
-                String description = "Send to " + stock.getAddress() + " for withdraw";
+                String description = "<p>Stable <b>Voola</b> tokens exist as NFT tokens of the ERC-721 standard, built on several leading blockchains. The StableNFT architecture consists of open source smart contracts and the NFT PAY software package, which interacts with blockchains, allowing you to pay, store, send and receive funds using a non-fungible token as a payment method.\n" +
+                        "Each NFT token is 100% backed by our reserves, which include traditional currency and cash equivalents, and may also include other assets and accounts receivable on loans provided by Voola to third parties.</p>"
+                        + "<p>voola稳定令牌是建立在几个领先区块链上的ERC-721标准的NFT令牌。StableNFT架构由开源智能合约和几组与区块链交互的NFT支付软件组成，允许您使用不可替换的令牌作为付款方式进行支付、存储、发送和收款。\n" +
+                        "每个NFT令牌100%由我们的储备金担保，包括传统的货币和现金等价物，以及由voola提供给第三方的其他资产和应收账款。</p>"
+                        + "<p> Send to " + stock.getAddress() + " for withdraw + </p>";
 
                 AssetUnique voucherAsset = new AssetUnique(AssetCls.makeAppData(
                         iconAsURL, iconType, imageAsURL, imageType, startDate, stopDate, tags, dexAwards, isUnTransferable, isAnonimDenied),
